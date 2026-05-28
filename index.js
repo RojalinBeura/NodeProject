0444c2fa1a521c209365d35c6003f7ac460a1b01
rojalin <bankanidhibeura@gmail.com>
2026-05-28 15:20:24 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello jenkin pipeline");
+console.log("Jenkins CI Pipeline Running");
+
+let a = 20;
+let b = 10;
+
+console.log("Addition:", a + b);
+console.log("Multiplication:", a * b);
+console.log("Subtraction:", a - b);
