bbc00c27d5c8fca2ed7aa9af0fcd1ce96f81fc72
rojalin <bankanidhibeura@gmail.com>
2026-05-29 17:41:56 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-   res.write("Hello from Jenkins dashboard");
+   res.write("Hello from Jenkin");
 
    res.end();
 
